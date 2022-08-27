846dddb52a5696e64e8b4cddaae9f6ee1eaef900
Chris Williams <chrispwill@gmail.com>
2022-08-27 17:41:16 +1000

    Add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+const rootElement = document.getElementById("app");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path={"/"} element={<App />}></Route>
+    </Routes>
+  </BrowserRouter>
+);
