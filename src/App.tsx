6f8bbbf7af02f0b794a444efe1efc049f7835409
Chris Williams <chrispwill@gmail.com>
2022-08-27 17:38:16 +1000

    Initial text

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import * as React from "react";
 import tw from "twin.macro";
 
-const Title = tw.div`text-3xl font-bold underline`;
+const Title = tw.div`text-3xl font-bold`;
+const SubTitle = tw.div`text-2xl`;
 
 export function App() {
-  return <Title>Hello world</Title>;
+  return (
+    <>
+      <Title>Chris Williams</Title>
+      <SubTitle>A software engineer's site</SubTitle>
+    </>
+  );
 }
