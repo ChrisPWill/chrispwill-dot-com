356b5c591d36517bca54f991cdb3b9ed04038ba7
Chris Williams <chrispwill@gmail.com>
2022-08-29 12:55:36 +1000

    Extract buttons into components library

---
+++ src/components/buttons.tsx
@@ -0,0 +1,4 @@
+import tw from "twin.macro";
+
+export const Button = tw.span`font-bold py-1.5 px-1.5 mx-1 rounded`;
+export const BlueButton = tw(Button)`bg-gray-500 hover:bg-gray-700 text-white`
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import tw from "twin.macro";
 import { Link, Outlet } from "react-router-dom";
 
+import { BlueButton } from "../components/buttons";
+
 const Title = tw.div`text-3xl font-bold`;
 const TopNav = tw.div`my-2`;
-const Button = tw.span`font-bold py-2 px-4 rounded mx-1`;
-const BlueButton = tw(Button)`bg-gray-500 hover:bg-gray-700 text-white`
 
 export function App() {
   return (
