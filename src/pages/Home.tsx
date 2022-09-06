ba4032fd42a6d3e984640cdcdc1c1d83fa86f0e4
Chris Williams <chrispwill@gmail.com>
2022-09-07 00:32:32 +1000

    Small test update to check deployment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,23 @@ export function Home() {
         <Heading>About me</Heading>
         <Content>
           Hi there. I'm Chris Williams. I'm a software engineer working in the
-          Ecosystem team at Atlassian.
+          Ecosystem team at Atlassian. In my spare time I like cooking,
+          travelling, and Japanese culture!
         </Content>
       </Section>
       <SectionDivider />
       <Section>
         <Heading>External Links</Heading>
-        <LinkStyle tw="underline"><a target="_blank" href="https://github.com/ChrisPWill">Github</a></LinkStyle>
-        <LinkStyle tw="underline"><a target="_blank" href="https://www.linkedin.com/in/chrispwill">LinkedIn</a></LinkStyle>
+        <LinkStyle tw="underline">
+          <a target="_blank" href="https://github.com/ChrisPWill">
+            Github
+          </a>
+        </LinkStyle>
+        <LinkStyle tw="underline">
+          <a target="_blank" href="https://www.linkedin.com/in/chrispwill">
+            LinkedIn
+          </a>
+        </LinkStyle>
       </Section>
     </>
   );
