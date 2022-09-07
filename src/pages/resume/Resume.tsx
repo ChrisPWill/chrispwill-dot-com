ff16db300214ce2b9abe71541e796b4c8dcda873
Chris Williams <chrispwill@gmail.com>
2022-09-07 13:55:27 +1000

    Improve certification display

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -5,6 +5,32 @@ import {
   Section,
   SectionDivider,
 } from "../../components/text";
+import tw from "twin.macro";
+
+const Certification = tw.span`
+  flex
+  rounded-lg
+  bg-gray-50
+  shadow-md
+`;
+
+const CertificationImage = tw.img`
+  my-auto
+  w-28
+  h-28
+`;
+
+const CertificationInfo = tw.div`
+  my-auto
+  mx-2
+`;
+
+const CertificationTitle = tw.div`
+  font-semibold
+`;
+
+const CertificationDescription = tw.div`
+`;
 
 export const Resume: React.FC = () => {
   return (
@@ -26,15 +52,23 @@ export const Resume: React.FC = () => {
       <SectionDivider />
       <Section>
         <Heading>Certifications</Heading>
-        <Content>
-          <a
-            target="_blank"
-            href="https://www.credly.com/badges/1dd7d7f7-f7aa-4999-b9e2-e8e4d3186ce8/public_url"
-            rel="noreferrer"
-          >
-            <img src="https://media.chrispwill.com/aws-certified-solutions-architect-associate.png" />
-          </a>
-        </Content>
+        <a
+          target="_blank"
+          href="https://www.credly.com/badges/1dd7d7f7-f7aa-4999-b9e2-e8e4d3186ce8/public_url"
+          rel="noreferrer"
+        >
+          <Certification>
+            <CertificationImage src="https://media.chrispwill.com/aws-certified-solutions-architect-associate.png" />
+            <CertificationInfo>
+              <CertificationTitle>
+                AWS Solutions Architect - Associate
+              </CertificationTitle>
+              <CertificationDescription>
+                Achieved on August 20, 2022
+              </CertificationDescription>
+            </CertificationInfo>
+          </Certification>
+        </a>
       </Section>
     </>
   );
